bfe4f7625ffe753b562e62300ef0996728832fce
Jin Lee <rosenwolf@gmail.com>
2023-12-02 18:21:14 -0800

    added getUserInput and mergeSlices

--- go/concurrency_in_go/week3_parallel_sort.go
+++ go/concurrency_in_go/week3_parallel_sort.go
@@ -6,20 +6,76 @@ The program should prompt the user to input a series of integers. Each goroutine
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sort"
+	"sync"
+)
+
+func getUserInput() []int {
+	fmt.Println("Enter integer numbers separted by a space")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+
+	values := strings.Split(scanner.Text(), " ")
+
+	userInput := make([]int, len(values))
+
+	for i, item  := range values {
+		inputInteger, err := strconv.Atoi(item)
+
+		if err == nil {
+			userInput[i] = inputInteger
+		}
+	}
+
+	return userInput
+}
+	
+func mergeSlices(slices ...[]int) []int {
+	var result [] int
+	for _, s := range slices {
+		result = append(result, s...)
+	}
+	sort.Ints(result)
+	return result
+}
 
 func main() {
 	// prompt the user to enter a series of integers
-
-	// read line from the user input
-
-	// split the string into an array of integers
+	originalSlice := getUserInput()
 
 	// split the array into 4 parts
+	n := len(originalSlice) / 4
+
+	var wg sync.WaitGroup
+	slices := make([][]int, 4)
 
 	// create 4 go routines and each go routine sorting each array
+	for i := 0; i < 4; i++ {
+		start := i * n
+		end := start + n
+		slices[i] = originalSlice[start:end]
+
+		// Sorting each slice in a goroutine
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			sort.Ints(slices[i])
+		}(i)
+	}
+
+	wg.Wait()
 
 	// main function waits for all go routines to finish and merge them into one single sorted array
 
 	//quick sort and merge sort?
+	sortedSlice := mergeSlices(slices[0],slices[1],slices[2],slices[3])
+	fmt.Println("Original input numbers:", originalSlice)	
+	fmt.Println("Sorted   input numbers:", sortedSlice)
 }
